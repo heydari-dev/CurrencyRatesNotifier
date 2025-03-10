dcbc8bfdbbe18a8c4314cbd2884fb33f636eff72
ali-heydari <ali.heydari.pg@gmail.com>
2025-03-10 04:09:15 -0700

    create archive for add rates

---
+++ config.py
@@ -0,0 +1,3 @@
+BASE_PATH = 'https://data.fixer.io/api/latest?access_key='
+API_KEY = 'e57bea001181aba9810477c2527c25ca'
+url = BASE_PATH + API_KEY
---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+from config import url
+import json
+
+def get_rates():
+    response = requests.get(url)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    return None
+
+def archive(filename, rates):
+    with open(f'archive/{filename}.json', 'w') as f:
+        f.write(json.dumps(rates))
+
+
+if __name__ == '__main__':
+    data = get_rates()
+    archive(data['timestamp'], data['rates'])
